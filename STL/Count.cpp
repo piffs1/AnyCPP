0a954638aef354650852faf4740702521f34ba53
piffs1 <nikiagafonov1@yandex.ru>
2019-06-15 14:23:33 +0300

    Comparing algorithm count with container(set) count

---
+++ STL/Count.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <set>
+#include <cassert>
+#include <algorithm>
+
+using namespace std;
+
+namespace STL_BOOK {
+
+}
+
+int main()
+{
+    set<int> setMy{2,42,17,14,52,68,11};
+    assert(setMy.count(42) == count(setMy.begin(), setMy.end(), 42));
+    /// set::count имеет сложность O(log N),
+    /// std::count имеет сложность O(N)
+    return 0;
+}
