4cb8733ff372414193e4e1af637ac200eb00970b
piffs1 <nikiagafonov1@yandex.ru>
2019-07-14 14:35:11 +0300

    iter_swap / ranges_swap explained

---
+++ STL/iter_swap.cpp
@@ -0,0 +1,43 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <string>
+#include <cassert>
+
+#define _STL_BOOK_BEGIN namespace STL_BOOK {
+#define _STL_BOOK_END }
+
+#define стоямба system("pause")
+
+/*
+
+*/
+
+_STL_BOOK_BEGIN
+
+template<class FwdIt1, class FwdIt2>
+void iter_swap(FwdIt1 it, FwdIt2 jt)
+{
+	std::swap(*it, *jt);
+}
+
+_STL_BOOK_END
+
+using namespace std;
+
+
+int main()
+{
+	setlocale(0, "");
+	
+	vector<int> vLocal1{ 1,2 };
+	vector<int> vLocal2{ 3,4 };
+	STL_BOOK::iter_swap(vLocal1.begin(), vLocal2.begin());
+	/// vLocal1{3,2}
+	/// vLocal2{1,4}
+	
+	стоямба;
+	return 0;
+}
+
---
+++ STL/swap_ranges.cpp
@@ -0,0 +1,45 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <string>
+#include <cassert>
+
+#define _STL_BOOK_BEGIN namespace STL_BOOK {
+#define _STL_BOOK_END }
+
+#define стоямба system("pause")
+
+/*
+
+*/
+
+_STL_BOOK_BEGIN
+
+template<class FwdIt1, class FwdIt2>
+FwdIt1 swap_ranges(FwdIt1 first1, FwdIt1 last1, FwdIt2 first2)
+{
+	while (first1 != last1) {
+		std::iter_swap(first1++, first2++);
+	}
+	return first2;
+}
+
+_STL_BOOK_END
+
+using namespace std;
+
+
+int main()
+{
+	setlocale(0, "");
+	vector<int> vLocal1{ 1,2,3,4 };
+	vector<int> vLocal2(vLocal1.size(), 0);
+
+	auto it = STL_BOOK::swap_ranges(vLocal1.begin(), vLocal1.begin() + 2, vLocal2.begin());
+	/// vLocal2 {1,2,0,0}
+	/// vLocal1 {0,0,3,4}
+	стоямба;
+	return 0;
+}
+
