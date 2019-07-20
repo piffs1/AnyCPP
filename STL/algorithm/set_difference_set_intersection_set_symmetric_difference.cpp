9f06c37465f96f2b56e5166c1e2aa596d3c235d7
piffs1 <nikiagafonov1@yandex.ru>
2019-07-20 11:03:25 +0300

    set_difference / set_intersection / set_symmetric_difference explained

--- STL/algorithm/set_difference_set_intersection_set_symmetric_difference.cpp
+++ STL/algorithm/set_difference_set_intersection_set_symmetric_difference.cpp
@@ -1,12 +1,42 @@
-﻿#include <iostream>
+﻿/* VS17 */
 
-using namespace std;
+#include <iostream>
+#include <algorithm>
+#include <list>
+#include <cassert>
+#include <string>
+#include <iterator>
+#define стоямба system("pause")
 
 namespace STL_BOOK {
 
 }
 
+using namespace std;
+
 int main()
 {
-    return 0;
+	setlocale(0, "");
+	
+	list<int> ltMainRange		{ 1,2,3,4,5 };
+	list<int> ltSubRange{ 1,  3,4, 6 };
+	
+	list<int> ltOutput;
+	set_difference(ltMainRange.begin(), ltMainRange.end(), 
+		ltSubRange.begin(), ltSubRange.end(), back_inserter(ltOutput));
+
+	// ltOutput {2,5}. Возвращает разницу только для первого контейнера со вторым
+	ltOutput.erase(ltOutput.begin(), ltOutput.end());
+	set_intersection(ltMainRange.begin(), ltMainRange.end(),
+		ltSubRange.begin(), ltSubRange.end(), back_inserter(ltOutput));
+
+	// ltOutput {1, 3, 4}. 
+	ltOutput.erase(ltOutput.begin(), ltOutput.end());
+
+	set_symmetric_difference(ltMainRange.begin(), ltMainRange.end(),
+		ltSubRange.begin(), ltSubRange.end(), back_inserter(ltOutput));
+	// ltOutput {2, 5, 6}. Возвращает разницу для двух контейнеров
+	стоямба;
+
+	return 0;
 }
