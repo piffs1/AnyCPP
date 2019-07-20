30323b83e81d9c9b14df1de1f3328f047f685b81
piffs1 <nikiagafonov1@yandex.ru>
2019-07-20 12:16:13 +0300

    minmax

---
+++ STL/algorithm/min_max.cpp
@@ -0,0 +1,31 @@
+﻿#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+namespace STL_BOOK {
+
+}
+
+int main()
+{
+	int nFirst = 5;
+	int nSecond = 6;
+	cout << max(nFirst, nSecond) << endl; /// 6
+	cout << min(nFirst, nSecond) << endl; /// 6
+	vector<int> vFirst{ 1,2,3,4,5,6 };
+	auto it = max_element(vFirst.begin(), vFirst.end()); /// 6
+	auto jt = min_element(vFirst.begin(), vFirst.end()); /// 1
+	pair<int, int> _pair = minmax(nFirst, nSecond);
+	assert(_pair.first == nFirst);
+	assert(_pair.second == nSecond);
+	_pair = minmax(nSecond, nFirst);
+	assert(_pair.first == nFirst);
+	assert(_pair.second == nSecond);
+
+	auto pair_its = minmax_element(vFirst.begin(), vFirst.end());
+	assert(*pair_its.first = nFirst);
+	assert(*pair_its.second == nSecond);
+	return 0;
+}
